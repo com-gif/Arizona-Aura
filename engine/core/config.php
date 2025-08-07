086e43608122be2ae4b44ad137a68b87af816665
com-gif <morlovm222@gmail.com>
2025-08-07 13:57:22 +0300

    Add files via upload

---
+++ engine/core/config.php
@@ -0,0 +1,22 @@
+<?php
+if(!class_exists('Config')) {
+    class Config {
+        const DB_HOST = '85.193.87.122';
+        const DB_USER = 'testbaze';
+        const DB_PASS = 'Qwerty123123';
+        const DB_NAME = 'testbaze';
+    }
+}
+
+$config = array(
+    'nameproject' => 'Arizona',
+    'url_group_vk' => 'https://vk.com/derryt',
+    'admin_name' => 'Admin',
+    'donat_x' => '2',
+    'email_name' => 'terryry@mail.ru',
+    'smtp' => '',
+    'smtp_port' => '',
+    'smtp_username' => '',
+    'smtp_password' => ''
+);
+?>
